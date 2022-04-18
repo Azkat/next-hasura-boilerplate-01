5f5f46ab113a0f0ba3025b254696989c4fcd14e6
Azkat <azkat.sg@gmail.com>
2022-04-18 23:44:04 +0900

    jwt認証までいった

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
+import '../styles/globals.css'
+import { useState } from 'react'
+import { useUserChanged } from '../hooks/useUserChanged'
 import { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const {} = useUserChanged()
+
   return <Component {...pageProps} />
 }
 
