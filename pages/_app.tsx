3eaaf35eed6e36a5f0dc390c50a5e0bc13ce6ab5
Azkat <azkat.sg@gmail.com>
2022-04-19 23:06:53 +0900

    initial deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,36 @@
 import '../styles/globals.css'
 import { useState } from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import { useUserChanged } from '../hooks/useUserChanged'
 import { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { store } from '../app/store'
+import { Hydrate } from 'react-query/hydration'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const {} = useUserChanged()
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: false,
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  )
+
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+        <ReactQueryDevtools />
+      </Hydrate>
+    </QueryClientProvider>
   )
 }
 
