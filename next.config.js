db419ae3d635172b190b43546488f3137ca11208
Azkat <azkat.sg@gmail.com>
2023-01-12 18:16:08 +0900

    nextconfig preview

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
   },
   async headers() {
     const headers = []
-    if (process.env.NEXT_PUBLIC_VERCEL_ENV == 'staging') {
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV == 'preview') {
       headers.push({
         headers: [
           {
