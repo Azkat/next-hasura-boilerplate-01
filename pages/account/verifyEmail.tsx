db8a708268bfe3468d22c34116fef4dd36717dc9
Azkat <azkat.sg@gmail.com>
2023-01-12 14:51:26 +0900

    bbb

--- pages/account/verifyEmail.tsx
+++ pages/account/verifyEmail.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router'
 import firebase, { auth } from '../../firebaseConfig'
 import { AuthContext } from '../../lib/authProvider'
 
-export default function verifyEmail(props) {
+export default function VerifyEmail(props) {
   const router = useRouter()
   const [isUser, setIsUser] = useState(false)
   const [isAudioFile, setIsAudioFile] = useState(false)
