3e669945c0de51d476228779511b4d86390b8f17
Azkat <azkat.sg@gmail.com>
2025-04-29 13:19:09 +0900

    env check

--- lib/graphql-client.ts
+++ lib/graphql-client.ts
@@ -1,6 +1,7 @@
 import { GraphQLClient } from 'graphql-request'
 
-console.log(process.env.NEXT_PUBLIC_HASURA_KEY)
+console.log('hasura key : ' + process.env.NEXT_PUBLIC_HASURA_KEY)
+console.log('hasura endpoint : ' + process.env.NEXT_PUBLIC_HASURA_ENDPOINT)
 
 export const graphQLClient = new GraphQLClient(
   process.env.NEXT_PUBLIC_HASURA_ENDPOINT || '',
